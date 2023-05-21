ae39c3047f6f7b296f2fea836fce49d45ee4f97d
Till Knollmann <till-knollmann@outlook.de>
2023-05-21 17:18:15 +0000

    Add skeleton for day 5

---
+++ aoc20/src/main/java/com/aoc20/Day05/Day05.java
@@ -0,0 +1,23 @@
+package com.aoc20.Day05;
+
+import com.aoc20.Day;
+
+public class Day05 extends Day {
+
+    public Day05() {
+        super(5);
+    }
+
+    @Override
+    public String solvePartOne(String input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'solvePartOne'");
+    }
+
+    @Override
+    public String solvePartTwo(String input) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'solvePartTwo'");
+    }
+
+}
